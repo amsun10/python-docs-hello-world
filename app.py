f3103f063619e07e26b3cdd10abf8d67e06fc21b
Xiang Zhang <xiang.zhang@citrix.com>
2021-07-20 19:59:18 +0800

    add redirect

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return redirect("http://localhost:5000/flag", code=302)
+
+
+if __name__ == '__main__':
+    app.run()
